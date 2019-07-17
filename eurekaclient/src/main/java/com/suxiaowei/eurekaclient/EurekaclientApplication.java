ac2e274f55de9916b9e23df7af0b69970d63a27c
“your_u <13262282692@163.com>
2019-07-17 16:52:43 +0800

    Eureka,config

--- eurekaclient/src/main/java/com/suxiaowei/eurekaclient/EurekaclientApplication.java
+++ eurekaclient/src/main/java/com/suxiaowei/eurekaclient/EurekaclientApplication.java
@@ -3,13 +3,21 @@ package com.suxiaowei.eurekaclient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @EnableEurekaClient
 @SpringBootApplication
 public class EurekaclientApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaclientApplication.class, args);
+	}
+
+	@RequestMapping("/hi")
+	public String sayHi(){
+		return "hi";
 	}
 
 }
